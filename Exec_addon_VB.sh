5eae62bcd7817b6cdfe8e6f7cfef1a8657d9c03c
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-03-23 20:17:51 +0100

    Add files via upload

---
+++ Exec_addon_VB.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Crée par : Kuroakashiro
+sudo mount /dev/cdrom /mnt
+if [ $? -gt 0 ]:
+then
+    echo -e "Verifier que vous avez bien ajoutée le CD addon de VB"
+    sleep 20
+    exit
+else
+    cd /mnt
+    sh ./VBoxLinuxAdditions.run
