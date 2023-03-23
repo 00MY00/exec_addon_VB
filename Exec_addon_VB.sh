0c853cf242757c701c63aa89a5bf2c545f55de4f
00MY00 <75865145+00MY00@users.noreply.github.com>
2023-03-23 20:27:56 +0100

    Add files via upload

--- Exec_addon_VB.sh
+++ Exec_addon_VB.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Crée par : Kuroakashiro
 sudo mount /dev/cdrom /mnt
-if [ $? -gt 0 ]:
+if [ $? -gt 0 ];
 then
     echo -e "Verifier que vous avez bien ajoutée le CD addon de VB"
     sleep 20
